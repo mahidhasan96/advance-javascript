e81c44df7d0594efed2074df8eade1bb053f3138
mahidhasan96 <mahidhasan1122@gmail.com>
2020-08-06 23:21:18 +0600

    null-undefine

---
+++ null-undefine.js
@@ -0,0 +1,20 @@
+//let pakhi;
+
+// function addNumber(num1, num2){
+//     console.log(num1 + num2);
+//     return
+// }
+
+// function addItem(item1, item2){
+//     console.log(item1, item2);
+// }
+
+
+// const premika = {name: "MAHID", phn: 015445444}
+// console.log(premika.gf);
+
+// let fun = undefined;
+// console.log(fun);
+let age = [100, 45, 145];
+console.log(age[4]);
+
