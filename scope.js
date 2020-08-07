95077e13233b97c3a8475eecac43318d93cfd597
mahidhasan96 <mahidhasan1122@gmail.com>
2020-08-07 19:19:57 +0600

    scope adding

---
+++ scope.js
@@ -0,0 +1,21 @@
+
+let bonus = 100;
+function sum(first, second){
+   
+    let result =first + second +bonus;
+     console.log(bonus);
+     if(result > 80){
+         var mood = "happy"
+         mood = "hasan";
+         mood = "mahid";
+         mood = "hi do you need it"
+        //  console.log(mood);
+     }
+    return result;
+}
+const output = sum(10,20);
+
+console.log(day);
+var  day = "firday";
+// console.log(bonus);
+// console.log(output);
