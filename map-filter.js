3e11b2afa2d6f7744f910d6907a49a079d3794ff
mahidhasan96 <mahidhasan1122@gmail.com>
2020-08-07 12:22:05 +0600

    add math-filter

--- map-filter.js
+++ map-filter.js
@@ -1,10 +1,23 @@
 const numbers = [41, 14, 52, 50, 74, 20];
 
-const output = [];
+// const output = [];
 
-for(let i = 0; i < numbers.length; i++){
-    const element = numbers[i];
-    const result = element * element;
-    output.push(result);
-}
-console.log(output);
+// for(let i = 0; i < numbers.length; i++){
+//     const element = numbers[i];
+//     const result = element * element;
+//     output.push(result);
+// }
+
+// function square(element){
+//     return element * element;
+// }
+//  const square = element => element * element;
+//  const square = x => x * y;
+
+//  const result = numbers.map(x => x * x)
+// console.log(result);
+
+
+const result = numbers.filter(x => x < 40);
+const isYes = numbers.find(x => x > 41);
+console.log(isYes);
